19e2e69a89ba2db978e66acc4e12bfc4c1f4219a
kyuuzou <newita@gmail.com>
2021-09-04 14:58:03 +0100

    Implement main GameSwitcher window, with underlying configuration for game names.

---
+++ Editor/GameSwitcher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace NelsonRodrigues.GameSwitcher {
+    public class GameSwitcher : EditorWindow {
+        private static readonly string ConfigurationResource = "GameSwitcherConfiguration.asset";
+        private GameSwitcherConfiguration configuration;
+        private Editor configurationEditor;
+        private bool showConfiguration = false;
+        
+        private DirectoryInfo skinsFolder;
+        
+        [MenuItem("Window/Game Switcher", false, (int)'G')]
+        public static void ShowNextToInspector() {
+            Assembly editorAssembly = typeof(Editor).Assembly;
+            Type inspectorWindowType = editorAssembly.GetType("UnityEditor.InspectorWindow");
+            
+            GameSwitcher switcher = EditorWindow.GetWindow<GameSwitcher>(
+                "Game Switcher", 
+                true, 
+                inspectorWindowType
+            );
+        }
+
+        private void InitialiseConfiguration() {
+            if (this.configuration != null) {
+                return;
+            }
+
+            this.configuration = (GameSwitcherConfiguration)EditorGUIUtility.Load(
+                GameSwitcher.ConfigurationResource
+            );
+
+            if (this.configuration == null) {
+                this.configuration = ScriptableObject.CreateInstance<GameSwitcherConfiguration>();
+                
+                AssetDatabase.CreateAsset(
+                    this.configuration,
+                    "Assets/Editor Default Resources/" + GameSwitcher.ConfigurationResource
+                );
+                
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private void OnEnable() {
+            this.InitialiseConfiguration();
+            this.configurationEditor = Editor.CreateEditor(this.configuration);
+
+            this.skinsFolder = new DirectoryInfo(Application.dataPath + "/../../Skins/");
+        }
+        
+        private void OnGUI() {
+            using (new GUILayout.VerticalScope()) {
+                this.showConfiguration = EditorGUILayout.Foldout(this.showConfiguration, "Configuration");
+                
+                if (this.showConfiguration) {
+                    this.configurationEditor.OnInspectorGUI();
+                }
+            }
+                        
+            this.Repaint();
+        }
+    }
+}
---
+++ Editor/GameSwitcherConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NelsonRodrigues.GameSwitcher {
+    public class GameSwitcherConfiguration : ScriptableObject {
+        
+        [SerializeField]
+        private List<string> gameNames;
+        public List<string> GameNames => this.gameNames;
+    }
+}
