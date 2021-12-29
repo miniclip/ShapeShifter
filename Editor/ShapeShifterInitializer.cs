66a0c02fed7baefd1946af7b0a51aad61a6d1ca4
João Vieira <joao.vieira@miniclip.com>
2021-12-29 10:40:14 +0000

    testing callbacks

--- Editor/ShapeShifterInitializer.cs
+++ Editor/ShapeShifterInitializer.cs
@@ -1,17 +1,25 @@
-﻿using Miniclip.ShapeShifter;
+using Miniclip.ShapeShifter;
 using Miniclip.ShapeShifter.Utils;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 [InitializeOnLoad]
-public class ShapeShifterInitializer : MonoBehaviour
+public class ShapeShifterInitializer
 {
     static ShapeShifterInitializer()
     {
+        Debug.Log("##! InitializeOnLoad");
         if (ShapeShifterEditorPrefs.GetBool(ShapeShifter.IsInitializedKey)) //TODO: To be changed for a settings provider?
         {
             ShapeShifter.InitializeShapeShifterCore();
             ShapeShifter.RestoreMissingAssets();
         }
+    }
+
+    [DidReloadScripts]
+    static void ScriptsReloaded()
+    {
+        Debug.Log("##! Scripts Reloaded");
     }
 }
