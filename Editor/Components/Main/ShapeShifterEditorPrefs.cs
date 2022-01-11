246311c279c411129062edeb4fcb021769b5d48d
João Vieira <joao.vieira@miniclip.com>
2022-01-11 01:50:34 +0000

    Organising scripts

--- Editor/Components/Main/ShapeShifterEditorPrefs.cs
+++ Editor/Components/Main/ShapeShifterEditorPrefs.cs
@@ -16,19 +16,19 @@ namespace Miniclip.ShapeShifter.Utils
 
         private static string UniqueProjectID => Path.GetFullPath(Application.dataPath);
 
-        private static string GetProjectSpecificKey(string key) => ShapeShifterEditorPrefs.UniqueProjectID + "_shapeshifter_" + key;
+        private static string GetProjectSpecificKey(string key) => UniqueProjectID + "_shapeshifter_" + key;
 
         [MenuItem("Window/Shape Shifter/Clear Editor Prefs", false, 71)]
         public static void OpenShapeShifter() => EditorPrefs.DeleteAll();
 
-        internal static bool HasKey(string key) => EditorPrefs.HasKey(ShapeShifterEditorPrefs.GetProjectSpecificKey(key));
+        internal static bool HasKey(string key) => EditorPrefs.HasKey(GetProjectSpecificKey(key));
 
-        internal static int GetInt(string key) => EditorPrefs.GetInt(ShapeShifterEditorPrefs.GetProjectSpecificKey(key));
+        internal static int GetInt(string key) => EditorPrefs.GetInt(GetProjectSpecificKey(key));
 
-        internal static void SetInt(string key, int value) => EditorPrefs.SetInt(ShapeShifterEditorPrefs.GetProjectSpecificKey(key), value);
+        internal static void SetInt(string key, int value) => EditorPrefs.SetInt(GetProjectSpecificKey(key), value);
 
-        public static bool GetBool(string key) => EditorPrefs.GetBool(ShapeShifterEditorPrefs.GetProjectSpecificKey(key));
+        public static bool GetBool(string key) => EditorPrefs.GetBool(GetProjectSpecificKey(key));
 
-        public static void SetBool(string key, bool value) => EditorPrefs.SetBool(ShapeShifterEditorPrefs.GetProjectSpecificKey(key), value);
+        public static void SetBool(string key, bool value) => EditorPrefs.SetBool(GetProjectSpecificKey(key), value);
     }
 }
