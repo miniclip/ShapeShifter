01292991b34e93b8934da3e18b11ebbfd55b18de
João Vieira <joao.vieira@miniclip.com>
2022-01-27 12:23:16 +0000

    Add utils for editor elements styles

---
+++ Editor/Core/Utils/StyleUtils.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Miniclip.ShapeShifter.Utils
+{
+    internal class StyleUtils
+    {
+        internal static GUIStyle BoxStyle => GUI.skin.GetStyle("Box");
+        internal static GUIStyle ButtonStyle => GUI.skin.GetStyle("Button");
+        internal static GUIStyle LabelStyle => GUI.skin.GetStyle("Label");
+    }
+}
