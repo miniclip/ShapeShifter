913a175466da4986d01123f8e35f468adf4381a8
João Vieira <joao.vieira@miniclip.com>
2022-02-11 14:39:09 +0000

    Cleanup

--- Editor/Core/Configuration/ShapeShifterConstants.cs
+++ Editor/Core/Configuration/ShapeShifterConstants.cs
@@ -1,15 +1,11 @@
-using JetBrains.Annotations;
-
 namespace Miniclip.ShapeShifter
 {
     internal class ShapeShifterConstants
     {
-        [CanBeNull]
         internal const string SKINS_MAIN_FOLDER = "Skins";
         internal const string EXTERNAL_ASSETS_FOLDER = "external";
         internal const string INTERNAL_ASSETS_FOLDER = "internal";
-        
-        internal const string ACTIVE_GAME_PLAYER_PREFS_KEY = "ACTIVE_GAME_PLAYER_PREFS_KEY";
 
+        internal const string ACTIVE_GAME_PLAYER_PREFS_KEY = "ACTIVE_GAME_PLAYER_PREFS_KEY";
     }
 }
