a5f7b0c9671b8ccd208ac85056db2dc92777a479
João Vieira <joao.vieira@miniclip.com>
2022-02-11 12:17:41 +0000

    prevent deleting active game

--- Editor/Core/Configuration/ShapeShifterConfigurationEditor.cs
+++ Editor/Core/Configuration/ShapeShifterConfigurationEditor.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using Miniclip.ShapeShifter.Skinner;
-using Miniclip.ShapeShifter.Switcher;
 using Miniclip.ShapeShifter.Utils;
 using UnityEditor;
-using UnityEditorInternal;
 using UnityEngine;
 
 namespace Miniclip.ShapeShifter
@@ -14,9 +9,9 @@ namespace Miniclip.ShapeShifter
     [CustomEditor(typeof(ShapeShifterConfiguration))]
     public class ShapeShifterConfigurationEditor : Editor
     {
-        private SerializedProperty gameSkinsProperty;
         private ShapeShifterConfiguration configuration;
         private ReadOnlyCollection<string> gameNames;
+        private SerializedProperty gameSkinsProperty;
 
         private void OnEnable()
         {
@@ -26,43 +21,57 @@ namespace Miniclip.ShapeShifter
         public override void OnInspectorGUI()
         {
             gameNames = new ReadOnlyCollection<string>(configuration.GameNames.AsReadOnly());
-
+            var activeGameIndex = ShapeShifter.ActiveGame;
             using (new EditorGUILayout.VerticalScope())
             {
                 EditorGUILayout.PrefixLabel("Game Names", StyleUtils.LabelStyle);
 
                 for (int index = 0; index < gameNames.Count; index++)
                 {
-                    string gameName = gameNames[index];
-                    using (new EditorGUILayout.HorizontalScope(StyleUtils.BoxStyle))
-                    {
-                        EditorGUILayout.LabelField(gameName, StyleUtils.LabelStyle);
-
-                        if (GUILayout.Button("Rename", StyleUtils.ButtonStyle))
-                        {
-                            throw new NotImplementedException();
-                        }
-
-                        if (GUILayout.Button("Duplicate", StyleUtils.ButtonStyle))
-                        {
-                            string newGame = ShapeShifterUtils.GetUniqueTemporaryGameName(gameName);
-
-                            GameSkin gameSkin = new GameSkin(gameName);
-                            gameSkin.Duplicate(newGame);
-                            
-                            ShapeShifterConfiguration.AddGame(newGame);
-                        }
-
-                        if (GUILayout.Button("Delete", StyleUtils.ButtonStyle))
-                        {
-                            ShapeShifterConfiguration.RemoveGame(gameName, true);
-                        }
-                    }
+                    bool isActive = index == activeGameIndex;
+                    DrawGameOperationsGUI(index, isActive);
                 }
             }
 
             serializedObject.Update();
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawGameOperationsGUI(int index, bool isActive)
+        {
+            string gameName = gameNames[index];
+
+            using (new EditorGUILayout.HorizontalScope(StyleUtils.BoxStyle))
+            {
+                EditorGUILayout.LabelField(gameName, StyleUtils.LabelStyle);
+
+                if (GUILayout.Button("Rename", StyleUtils.ButtonStyle))
+                {
+                    throw new NotImplementedException();
+                }
+
+                if (GUILayout.Button("Duplicate", StyleUtils.ButtonStyle))
+                {
+                    string newGame = ShapeShifterUtils.GetUniqueTemporaryGameName(gameName);
+
+                    GameSkin gameSkin = new GameSkin(gameName);
+                    gameSkin.Duplicate(newGame);
+
+                    ShapeShifterConfiguration.AddGame(newGame);
+                }
+
+                if (GUILayout.Button("Delete", StyleUtils.ButtonStyle))
+                {
+                    if (!isActive)
+                    {
+                        ShapeShifterConfiguration.RemoveGame(gameName, true);
+                    }
+                    else
+                    {
+                        ShapeShifterLogger.LogWarning("Can't deleted a game skin while it is active");
+                    }
+                }
+            }
         }
     }
 }
