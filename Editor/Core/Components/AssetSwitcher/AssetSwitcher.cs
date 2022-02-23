07ebb81f4c24bf12b1ef839fa1f2a57b1ee68592
João Vieira <joao.vieira@miniclip.com>
2022-02-23 17:57:44 +0000

    Add TODO with ticket ID

--- Editor/Core/Components/AssetSwitcher/AssetSwitcher.cs
+++ Editor/Core/Components/AssetSwitcher/AssetSwitcher.cs
@@ -383,14 +383,15 @@ namespace Miniclip.ShapeShifter.Switcher
             ShapeShifter.ActiveGame = gameToSwitchTo.Name;
             ShapeShifterConfiguration.Instance.SetDirty(false);
 
-            // GameSkin gameSkin = ShapeShifter.ActiveGameSkin;
-            //
-            // foreach (AssetSkin assetSkin in gameSkin.GetAssetSkins())
-            // {
-            //     string guid = assetSkin.Guid;
-            //
-            //     AssetDatabase.ImportAsset(AssetDatabase.GUIDToAssetPath(guid), ImportAssetOptions.ForceUpdate);
-            // }
+            //TODO: ACPT-2843 make the code below optionable
+            /*GameSkin gameSkin = ShapeShifter.ActiveGameSkin;
+            
+            foreach (AssetSkin assetSkin in gameSkin.GetAssetSkins())
+            {
+                string guid = assetSkin.Guid;
+            
+                AssetDatabase.ImportAsset(AssetDatabase.GUIDToAssetPath(guid), ImportAssetOptions.ForceUpdate);
+            }*/
         }
 
         private static void CopyIfMissingInternal(DirectoryInfo directory)
